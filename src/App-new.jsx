eb539ef54cb34e77312bd7077d26548b0797ef12
Khuongvjppro <22130129@st.hcmuaf.edu.vn>
2025-12-24 10:11:42 +0700

    route

--- src/App-new.jsx
+++ src/App-new.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { CartProvider } from "./contexts/CartContext";
 import HomePage from "./pages/HomePage";
 import ProductDetail from "./pages/ProductDetail";
@@ -17,7 +22,6 @@ function App() {
         <Routes>
           <Route path={ROUTES.LOGIN} element={<Login />} />
           <Route path={ROUTES.HOME} element={<HomePage />} />
-          <Route path="/home" element={<Navigate to={ROUTES.HOME} replace />} />
           <Route path={ROUTES.PRODUCT_DETAIL} element={<ProductDetail />} />
           <Route path={ROUTES.NEWS} element={<NewsPage />} />
           <Route path={ROUTES.CART} element={<Cart />} />
