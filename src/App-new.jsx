574877d9cfe0f8e362a3292af1676abb0990a628
M2K1423 <164112701+M2K1423@users.noreply.github.com>
2025-12-25 10:24:49 +0700

    chore: cập nhật routing cho các trang mới

--- src/App-new.jsx
+++ src/App-new.jsx
@@ -5,6 +5,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import { CartProvider } from "./contexts/CartContext";
+import { AuthProvider, useAuth } from "./contexts/AuthContext";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
 import ProductDetail from "./pages/ProductDetail";
@@ -14,35 +15,42 @@ import OrderProcessPage from "./pages/OrderProcessPage";
 import PaymentMethodsPage from "./pages/PaymentMethodsPage";
 import Login from "./pages/Login";
 import { ROUTES } from "./constants";
+import OrderConfirm from "./pages/OrderConfirm";
+import CheckoutInfo from "./pages/CheckoutInfo";
+import CheckoutConfirm from "./pages/CheckoutConfirm";
 import "./styles/global.css";
+
+function RequireAuth({ children }) {
+  const { user } = useAuth();
+  if (!user) {
+    return <Navigate to={ROUTES.LOGIN} replace />;
+  }
+  return children;
+}
 
 function App() {
   return (
-    <CartProvider>
-      <Router>
-        <Routes>
-          <Route path={ROUTES.LOGIN} element={<Login />} />
-          <Route path={ROUTES.HOME} element={<HomePage />} />
-          <Route path="/products" element={<ProductsPage />} />
-          <Route path={ROUTES.PRODUCT_DETAIL} element={<ProductDetail />} />
-          <Route path={ROUTES.NEWS} element={<NewsPage />} />
-          <Route path={ROUTES.CART} element={<Cart />} />
-          <Route path={ROUTES.ORDER_PROCESS} element={<OrderProcessPage />} />
-          <Route
-            path={ROUTES.CONSULTATION_ORDER_PROCESS}
-            element={<OrderProcessPage />}
-          />
-          <Route
-            path={ROUTES.PAYMENT_METHODS}
-            element={<PaymentMethodsPage />}
-          />
-          <Route
-            path={ROUTES.CONSULTATION_PAYMENT_METHODS}
-            element={<PaymentMethodsPage />}
-          />
-        </Routes>
-      </Router>
-    </CartProvider>
+    <AuthProvider>
+      <CartProvider>
+        <Router>
+          <Routes>
+            <Route path={ROUTES.HOME} element={<HomePage />} />
+            <Route path={ROUTES.LOGIN} element={<Login />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path={ROUTES.PRODUCT_DETAIL} element={<ProductDetail />} />
+            <Route path={ROUTES.NEWS} element={<NewsPage />} />
+            <Route path={ROUTES.CART} element={<Cart />} />
+            <Route path={ROUTES.ORDER_PROCESS} element={<RequireAuth><OrderProcessPage /></RequireAuth>} />
+            <Route path="/order-confirm" element={<RequireAuth><OrderConfirm /></RequireAuth>} />
+            <Route path={ROUTES.CONSULTATION_ORDER_PROCESS} element={<RequireAuth><OrderProcessPage /></RequireAuth>} />
+            <Route path={ROUTES.PAYMENT_METHODS} element={<RequireAuth><PaymentMethodsPage /></RequireAuth>} />
+            <Route path={ROUTES.CONSULTATION_PAYMENT_METHODS} element={<RequireAuth><PaymentMethodsPage /></RequireAuth>} />
+              <Route path="/checkout-info" element={<RequireAuth><CheckoutInfo /></RequireAuth>} />
+            <Route path="/checkout-confirm" element={<RequireAuth><CheckoutConfirm /></RequireAuth>} />
+          </Routes>
+        </Router>
+      </CartProvider>
+    </AuthProvider>
   );
 }
 
